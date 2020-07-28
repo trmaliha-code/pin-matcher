9c9449b67e2835ccc16c6f6183264ce946115833
unknown <maliha1288@gmail.com>
2020-07-28 23:21:36 +0600

    updated

---
+++ script.js
@@ -0,0 +1,64 @@
+// Variable declaration
+var generatorBox = document.querySelector(".pin-generator-box");
+var inputBox = document.querySelector(".pin-input-box");
+
+
+// Pin generate section
+var number = 0;
+
+function numberGenerator()
+{
+    number = Math.random() * 10000;
+    number = Math.round(number);
+}
+
+document.querySelector(".generate-btn").addEventListener('click', function(){
+    numberGenerator();
+    while(number < 1000 || number > 9999)
+    {
+        numberGenerator();
+    }
+    generatorBox.value = number;
+});
+
+
+// Pin user input section
+var digitsConcat = "";
+
+var buttons = document.getElementsByClassName("button");
+
+for(var i = 0; i < buttons.length; i++)
+{  
+    buttons[i].addEventListener('click', function(){
+        if(this.id == "C")
+        {
+            digitsConcat = ""
+            inputBox.value = digitsConcat;
+        }
+        else if(this.id == "<")
+        {
+            digitsConcat = inputBox.value.substr(0, inputBox.value.length - 1);
+            inputBox.value = digitsConcat;
+        }
+        else
+        {
+            digitsConcat  = digitsConcat + this.id;
+            inputBox.value = digitsConcat;
+        }
+    });   
+}
+
+
+// Pin matching section
+document.querySelector(".submit-btn").addEventListener('click', function(){
+    if(generatorBox.value == inputBox.value)
+    {
+        document.getElementById("matched").style.display = "block";
+        document.getElementById("not-matched").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("not-matched").style.display = "block";
+        document.getElementById("matched").style.display = "none";
+    }
+});
